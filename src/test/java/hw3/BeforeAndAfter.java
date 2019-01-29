d44517a98102cdd9bcb5728bbc823d09cacc2547
Mikhail Kuchugurin <Mikhail.Kuchugurin@Clarivate.com>
2019-01-29 18:27:30 +0300

    third homework has been added

---
+++ src/test/java/hw3/BeforeAndAfter.java
@@ -0,0 +1,34 @@
+package hw3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class BeforeAndAfter {
+    protected WebDriver driver;
+    private String url = "https://epam.github.io/JDI/";
+
+
+
+    @BeforeSuite
+    public void openBrowser() {
+        //1 Run browser and open test site
+        System.setProperty("webdriver.chrome.driver", "\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+    }
+
+
+    @AfterSuite
+    public void closeBrowser(){
+        //17 close browser
+        driver.close();
+    }
+}
