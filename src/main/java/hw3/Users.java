b5787a2e61de89c8db450076d17f252f394182d9
Dmitry_Lebedev <Dmitry_Lebedev1@epam.com>
2019-01-31 22:17:13 +0300

    Comments from Dmitry

--- src/main/java/hw3/Users.java
+++ src/main/java/hw3/Users.java
@@ -4,6 +4,7 @@ public enum Users {
     PITER("epam", "1234", "PITER CHAILOVSKII");
     public String login;
     public String pass;
+    // TODO Take a look on java code convention, fields naming style.
     public String FirstAndLastName;
 
     Users(String login, String pass, String firstAndLastName) {
